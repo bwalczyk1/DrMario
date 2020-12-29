0f19a88d0727c25faeb7067403fe62a332770376
Bartosz Walczyk <walczykbartosz03@gmail.com>
2020-12-29 01:10:22 +0100

    Completed Stage 3

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 "use strict";
 let main = {
     GAMEOVER: false,
+    level: 3,
+    score: 0,
     load: function(){
+        if(!localStorage.getItem("top"))
+            localStorage.setItem("top", 0);
         document.body.style.position = "relative";
         let tab = document.createElement("table");
         tab.style.borderCollapse = "collapse";
@@ -22,6 +26,35 @@ let main = {
             }
         }
         document.body.appendChild(tab);
+        let p1 = document.createElement("p");
+        p1.id = "p1";
+        p1.style.position = "absolute";
+        p1.style.top = "500px";
+        p1.style.left = "100px";
+        p1.innerHTML = "TOP: " + localStorage.getItem("top") + "<br>SCORE: " + main.score;
+        document.body.appendChild(p1);
+        main.createViruses();
         pill.start();
+    },
+    createViruses: function(){
+        let x, y, id;
+        let virusIds = new Array();
+        let virusElement;
+        do{
+            x = Math.floor(Math.random() * 8);
+            y = Math.floor(Math.random() * 12) + 4;
+            id = x + "_" + y;
+            if(!virusIds.includes(id)){
+                virusElement = document.getElementById(id)
+                virusElement.className = "virus";
+                virusElement.innerHTML = "V";
+                if(document.getElementsByClassName("virus").length % 3 == 1)
+                    virusElement.style.backgroundColor = "red";
+                else if(document.getElementsByClassName("virus").length % 3 == 2)
+                    virusElement.style.backgroundColor = "yellow";
+                else if(document.getElementsByClassName("virus").length % 3 == 0)
+                    virusElement.style.backgroundColor = "blue";
+            }
+        }while(document.getElementsByClassName("virus").length < main.level * 4 + 4);
     }
 }
--- pill.js
+++ pill.js
@@ -9,6 +9,8 @@ let pill = {
     color2: "white",
     falling: false,
     pressingKey: false,
+    gravity: false,
+    atLeastOnePulledDown: false,
     start: function(){
         document.body.addEventListener("keydown", pill.checkKey);
         document.body.addEventListener("keyup", ()=>{
@@ -45,6 +47,7 @@ let pill = {
     new: function(){
         this.create();
         this.falling = false;
+        this.gravity = false;
         setTimeout(this.move2, 600);
     },
     create: function(){
@@ -67,7 +70,18 @@ let pill = {
         else return "blue";
     },
     move1: function(id1){
-
+        if(parseInt(id1.substring(2, id1.length)) < 15){
+            console.log(id1);
+            let newId1 = id1.substring(0, 2) + (parseInt(id1.substring(2, id1.length)) + 1);
+            console.log(newId1);
+            if(document.getElementById(newId1).className == "empty"){
+                document.getElementById(newId1).className = document.getElementById(id1).className;
+                document.getElementById(newId1).style.backgroundColor = document.getElementById(id1).style.backgroundColor;
+                document.getElementById(id1).className = "empty";
+                document.getElementById(id1).style.backgroundColor = "white";
+                pill.atLeastOnePulledDown = true;
+            }
+        }
     },
     move2: function(id1 = "0", id2 = "0"){
         if(id1 == "0" && id2 == "0"){
@@ -91,29 +105,34 @@ let pill = {
                 document.getElementById(newId2).style.backgroundColor = document.getElementById(id2).style.backgroundColor;
                 document.getElementById(id2).className = "empty";
                 document.getElementById(id2).style.backgroundColor = "white";
-                pill.mainPositionX = parseInt(newId1.substring(0, 1));
-                pill.mainPositionY = parseInt(newId1.substring(2, newId1.length));
-                pill.sidePositionX = parseInt(newId2.substring(0, 1));
-                pill.sidePositionY = parseInt(newId2.substring(2, newId2.length));
-                if(pill.falling)
-                    setTimeout(pill.move2, 40);
+                if(!pill.gravity){
+                    pill.mainPositionX = parseInt(newId1.substring(0, 1));
+                    pill.mainPositionY = parseInt(newId1.substring(2, newId1.length));
+                    pill.sidePositionX = parseInt(newId2.substring(0, 1));
+                    pill.sidePositionY = parseInt(newId2.substring(2, newId2.length));
+                    if(pill.falling)
+                        setTimeout(pill.move2, 50);
+                    else
+                        setTimeout(pill.move2, 600);
+                }
                 else
-                    setTimeout(pill.move2, 600);
+                    pill.atLeastOnePulledDown = true;
             }
-            else{
-                if(parseInt(id1.substring(2, id1.length)) == 0){
+            else if(!pill.gravity){
+                if(newId1 == "3_1" && newId2 == "4_1"){
                     console.log("GAME OVER");
+                    if(main.score > localStorage.getItem("top")){
+                        localStorage.setItem("top", main.score);
+                    }
                     main.GAMEOVER = true;
                 }   
                 else{
                     pill.delete4();
-                    pill.new();
                 }
             } 
         }
-        else{
+        else if(!pill.gravity){
             pill.delete4();
-            pill.new();
         }
     },
     delete4: function(){
@@ -122,7 +141,7 @@ let pill = {
         for(let i = 0; i < 8; i++){
             for(let j = 0; j < 16; j++){
                 id = i + "_" + j;
-                if(i <= 4 ){
+                if(i <= 4){
                     if(document.getElementById(id).className != "empty" 
                     && document.getElementById(id).style.backgroundColor == document.getElementById((i + 1) + "_" + j).style.backgroundColor
                     && document.getElementById((i + 1) + "_" + j).style.backgroundColor == document.getElementById((i + 2) + "_" + j).style.backgroundColor
@@ -142,11 +161,63 @@ let pill = {
                 }
             }
         }
-        for(let i = 0; i < toDeleteArr.length; i++){
-            id = toDeleteArr[i];
-            document.getElementById(id).className = "empty";
-            document.getElementById(id).style.backgroundColor = "white";
+        if(toDeleteArr.length > 0){
+            for(let i = 0; i < toDeleteArr.length; i++){
+                id = toDeleteArr[i];
+                if(document.getElementById(id).className == "virus"){
+                    main.score += 100;
+                    document.getElementById("p1").innerHTML = "TOP: " + localStorage.getItem("top") + "<br>SCORE: " + main.score;
+                }
+                document.getElementById(id).className = "empty";
+                document.getElementById(id).style.backgroundColor = "white";
+                document.getElementById(id).innerHTML = "";
+            }
+            setTimeout(pill.gravityForce, 50);
         }
+        else{
+            if(document.getElementsByClassName("virus").length == 0){
+                console.log("STAGE COMPLETED");
+                if(main.score > localStorage.getItem("top")){
+                    localStorage.setItem("top", main.score);
+                }
+            }
+            else
+                pill.new();
+        }
+    },
+    gravityForce: function(){
+        pill.gravity = true;
+        pill.atLeastOnePulledDown = false;
+        let testedClassNames = new Array();
+        let testedClassName;
+        let x;
+        let id0, id1;
+        for(let i = 15; i >= 0; i--){
+            for(let j = 7; j >= 0; j--){
+                testedClassName = document.getElementById(j + "_" + i).className;
+                if(testedClassName != "empty" && testedClassName != "virus"){
+                    if(!testedClassNames.includes(testedClassName)){
+                        x = document.getElementsByClassName(testedClassName).length;
+                        if(x == 1){
+                            pill.move1(document.getElementsByClassName(testedClassName)[0].id);
+                        }
+                        else if(x == 2){
+                            id0 = document.getElementsByClassName(testedClassName)[0].id;
+                            id1 = document.getElementsByClassName(testedClassName)[1].id;
+                            if(id1 == id0.substring(0, 2) + (parseInt(id0.substring(2, id0.length)) + 1))
+                                pill.move2(document.getElementsByClassName(testedClassName)[1].id, document.getElementsByClassName(testedClassName)[0].id);
+                            else if(id1 == (parseInt(id0.substring(0, 1)) + 1) + id0.substring(1, id0.length))
+                                pill.move2(document.getElementsByClassName(testedClassName)[0].id, document.getElementsByClassName(testedClassName)[1].id);
+                        }
+                        testedClassNames.push(testedClassName);
+                    }
+                }
+            }
+        }
+        if(pill.atLeastOnePulledDown)
+            setTimeout(pill.gravityForce, 50);
+        else
+            pill.delete4();
     },
     moveRight: function(id1, id2){
         if(parseInt(id2.substring(0, 1)) < 7){
